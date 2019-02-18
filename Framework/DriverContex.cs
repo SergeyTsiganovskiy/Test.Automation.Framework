b2f3391629504f995e0a2176f2daf754b704dc38
SergeyTsiganovskiy <stsyganivskyi@deloitte.com>
2019-02-18 18:29:29 +0200

    Add class for initiating webdriver

---
+++ Framework/DriverContex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using NLog;
+
+namespace Framework
+{
+    class DriverContex
+    {
+        private static readonly Logger Logger = LogManager.GetLogger("DRIVER");
+        private readonly Collection<ErrorDetail> verifyMessages = new Collection<ErrorDetail>();
+    }
+}
---
+++ Framework/ErrorDetail.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace Framework
+{
+    public class ErrorDetail
+    {
+        /// <summary>
+        /// Gets or sets the screenshot.
+        /// </summary>
+        /// <value>
+        /// The screenshot.
+        /// </value>
+        public Screenshot Screenshot { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date time.
+        /// </summary>
+        /// <value>
+        /// The date time.
+        /// </value>
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the exception.
+        /// </summary>
+        /// <value>
+        /// The exception.
+        /// </value>
+        public Exception Exception { get; set; }
+
+        public ErrorDetail(Screenshot screenshot, DateTime dateTime, Exception exception)
+        {
+            this.Screenshot = screenshot;
+            this.DateTime = dateTime;
+            this.Exception = exception;
+        }
+    }
+}
